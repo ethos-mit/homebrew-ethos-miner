40fda0d0f697e8dcd5d26e70f7fc712ee9097bb1
Guy Zyskind <guy@zyskind.com>
2014-08-22 10:16:49 -0400

    Ethos Miner brew

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-homebrew-ethos-miner
-====================
+homebrew-ethos
+==============
+
+Homebrew tap for Ethos.
+
+Tested on 10.9+
+
+
+## Installation
+
+Homebrew is required for the installation.
+
+Start with tapping into ethos' brew:
+
+```
+brew tap ethos-mit/ethos
+brew install ethos
+```
+
+Set ETHOS_TOKEN with the token you received:
+
+```
+export ETHOS_TOKEN=<token>
+```
+
+Install ethos:
+```
+brew install ethos
+```
+
+
+## Running
+
+Connecting to ethos test-net:
+
+```
+ethosd --node 18.85.59.189 --port 30307
+```
+
+To get help and see other available arguments, run:
+
+```
+ethosd --help
+```
---
+++ ethos-miner.rb
@@ -0,0 +1,65 @@
+require 'formula'
+
+class EthosMiner < Formula
+
+  version '0.1'
+  homepage ""
+
+  url 'http://officebob.media.mit.edu:8888/miner?token=' + ENV['ETHOS_TOKEN']
+  # url 'http://127.0.0.1:8888/miner?token=' + ENV['ETHOS_TOKEN']
+  sha1 "" # TODO: fill out before deployment
+
+  depends_on 'cmake' => :build
+  depends_on 'boost' => ["c++11", "with-python"]
+  # depends_on 'pkg-config' => :build
+  depends_on 'qt5'
+  depends_on 'cryptopp'
+  depends_on 'miniupnpc'
+  depends_on 'leveldb'
+  depends_on 'gmp'
+  depends_on 'curl'
+  depends_on 'jsonrpc'
+
+  option "with-local", "Url is local" # TODO
+  option 'without-jsonrpc', "Build without JSON-RPC dependency"
+  option "without-paranoia", "Build with -DPARANOIA=0"
+  option 'with-vmtrace', "Build with VMTRACE"
+
+  def install
+    args = *std_cmake_args, "-DLANGUAGES=0"
+    args << "-DCMAKE_BUILD_TYPE=brew"
+    args << "-DVMTRACE=1" if build.include? "with-vmtrace"
+    args << "-DPARANOIA=0" if build.include? "without-paranoia"
+
+
+    # handle ethos
+    if File.exist?('~/.pydistutils.cfg')
+      system "cp ~/.pydistutils.cfg ~/.pydistutils.cfg.orig"
+    end
+
+    system "printf '[install]\ninstall_lib = ~/Library/Python/$py_version_short/lib/python/site-packages' > ~/.pydistutils.cfg"
+    system "mkdir -p ~/Library/Python/2.7/lib/python/site-packages"
+    system "easy_install ./ethos-0.1-py2.7.egg"
+    system "rm ~/.pydistutils.cfg"
+
+    if File.exist?('~/.pydistutils.cfg.orig')
+      system "mv ~/.pydistutils.cfg.orig ~/.pydistutils.cfg"
+    end
+
+    # handle ethreum
+    system "cmake", *args
+    system "make"
+    system "make", "install"
+
+    # NOTE: work on this in order to create symlinks to ethos version
+    # bin.install_symlink bin/"eth"
+
+  end
+
+  test do
+    system "ethosd"
+    system "eth"
+  end
+
+end
+__END__
